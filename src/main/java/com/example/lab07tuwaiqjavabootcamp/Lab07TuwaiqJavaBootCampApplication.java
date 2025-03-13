d1537a34b36b4876eee57a56c132442c7abf3ad6
aliali <aliali@test.com>
2025-03-13 10:20:48 +0300

    first commit

---
+++ src/main/java/com/example/lab07tuwaiqjavabootcamp/Lab07TuwaiqJavaBootCampApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab07tuwaiqjavabootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab07TuwaiqJavaBootCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab07TuwaiqJavaBootCampApplication.class, args);
+    }
+
+}
